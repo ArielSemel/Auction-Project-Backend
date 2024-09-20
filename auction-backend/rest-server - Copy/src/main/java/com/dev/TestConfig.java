9085362fcf7f45a19c2d111b6ef05fd132d9410e
ArielSemel <93810168+ArielSemel@users.noreply.github.com>
2024-09-20 14:17:45 +0300

    commited

--- auction-backend/rest-server - Copy/src/main/java/com/dev/TestConfig.java
+++ auction-backend/rest-server - Copy/src/main/java/com/dev/TestConfig.java
@@ -6,35 +6,48 @@ import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Environment;
 import org.hibernate.service.ServiceRegistry;
 import org.reflections.Reflections;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Properties;
 import java.util.Set;
 
-/**
- * Test config that turn on H2 in-memory database.
- * This mode is more convenient for fast starting.
- * Change property spring.profiles.active to "test" for run application in this mode.
- */
+
 
 @Configuration
 @Profile("production")
 public class TestConfig {
 
+
     @Bean
-    public Properties dataSource() throws Exception {
+    public Properties dataSource() {
+
         Properties settings = new Properties();
+
+        String dbUrl = System.getenv("DB_URL");
+        String dbUsername = System.getenv("DB_USERNAME");
+        String dbPassword = System.getenv("DB_PASSWORD");
+
+        if (dbUrl == null || dbUsername == null || dbPassword == null) {
+            throw new NullPointerException("Database environment variables are not set.");
+        }
+
         settings.put(Environment.DRIVER, "com.mysql.jdbc.Driver");
-        settings.put(Environment.URL, "jdbc:mysql://localhost:3306/auction?useSSL=false&amp;useUnicode=true&amp;characterEncoding=utf8");
-        settings.put(Environment.USER, "root");
-        settings.put(Environment.PASS, "1234");
+        settings.put(Environment.URL, System.getenv("DB_URL"));
+        settings.put(Environment.USER, System.getenv("DB_USERNAME"));
+        settings.put(Environment.PASS, System.getenv("DB_PASSWORD"));
         settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
         settings.put(Environment.SHOW_SQL, "true");
         settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
         settings.put(Environment.HBM2DDL_AUTO, "update");
         settings.put(Environment.ENABLE_LAZY_LOAD_NO_TRANS, true);
+
         return settings;
     }
 
